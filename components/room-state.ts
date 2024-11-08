630e45f18efaa42b26213bcfbabdef814c46f352
Kevin Wu <kevin@kevinniuwu.com>
2024-11-07 22:11:55 -0500

    Add hash, make receiver async

--- components/room-state.ts
+++ components/room-state.ts
@@ -9,9 +9,26 @@ export function setupRoomState(element: HTMLDivElement) {
     gameStateDiv.className = 'game-state'
     gameStateDiv.innerHTML = 'Game state: ...'
 
-    onMessage('room-state', (message) => {
+    onMessage('room-state', async (message) => {
         console.log('Received room-state message:', message.data)
-        gameStateDiv.innerHTML = `Game state: ${JSON.stringify(message.data)}`
+
+        const state = message.data! as any
+        let answers = ''
+        for (const cell of state.cells) {
+            answers += cell.answer ?? ' '
+        }
+
+        // hash the answers to get a unique identifier for the game state
+        const encoder = new TextEncoder()
+        const answerData = encoder.encode(answers)
+        const hash = await crypto.subtle.digest('SHA-256', answerData)
+        const hashArray = Array.from(new Uint8Array(hash))
+        const hashHex = hashArray
+            .map((b) => b.toString(16).padStart(2, '0'))
+            .join('')
+        console.log('Answer hash:', hashHex)
+
+        gameStateDiv.innerHTML = `Game state: ${hashHex}`
     })
 
     element.appendChild(connectedUsersDiv)
