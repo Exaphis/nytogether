8e8808fbfb3f4c1db71f899a59da4f9c586eeae2
Kevin Wu <kevin@kevinniuwu.com>
2024-11-09 23:51:07 -0500

    Add join room buttons

--- entrypoints/popup/main.ts
+++ entrypoints/popup/main.ts
@@ -13,6 +13,11 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <div class="room-input">
         <label for="room-name">Room Name:</label>
         <input type="text" id="room-name" placeholder="Enter room name">
+        <div class="auto-join">
+          <input type="checkbox" id="auto-join">
+          <label for="auto-join">Auto-join</label>
+        </div>
+        <button id="join-button">Join</button>
       </div>
       <div id="room-state">
         <h2>Room State</h2>
@@ -23,11 +28,35 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
 setupRoomState(document.querySelector<HTMLDivElement>('#room-state')!)
 
+function sendMessageToTab(messageID: string, data: any) {
+    browser.tabs.query({ active: true, currentWindow: true }, (tabs: any) => {
+        if (tabs.length > 0) {
+            sendMessage(messageID, data, `content-script@${tabs[0].id}`)
+        }
+    })
+}
+
+document
+    .querySelector<HTMLInputElement>('#room-name')!
+    .addEventListener('input', (event) => {
+        sendMessageToTab('set-room-name', {
+            roomName: (event.target as HTMLInputElement).value,
+        })
+    })
+
+document
+    .querySelector<HTMLInputElement>('#join-button')!
+    .addEventListener('click', () => {
+        sendMessageToTab('join-room', {})
+    })
+
+document
+    .querySelector<HTMLInputElement>('#auto-join')!
+    .addEventListener('change', (event) => {
+        sendMessageToTab('set-auto-join', {
+            autoJoin: (event.target as HTMLInputElement).checked,
+        })
+    })
+
 log('Querying room-state')
-browser.tabs.query({ active: true, currentWindow: true }, (tabs: any) => {
-    if (tabs.length > 0) {
-        const tabId = tabs[0].id
-        log('Querying room-state for tab', tabId)
-        sendMessage('query-room-state', {}, `content-script@${tabId}`)
-    }
-})
+sendMessageToTab('query-room-state', {})
