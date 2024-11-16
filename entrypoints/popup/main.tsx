23383cfcfacabd770e5f2afd27b8cb26228f0907
Kevin Wu <kevin@kevinniuwu.com>
2024-11-16 14:23:35 -0500

    Add room state hook

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -1,34 +1,85 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './tailwind.css'
-import { Button } from '@/components/ui/button'
+import { onMessage, sendMessage } from 'webext-bridge/popup'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 
-function Popup() {
-    const [count, setCount] = React.useState(10)
+const log = (message: string, ...args: any[]) => {
+    console.log(`[NYTogether/popup] ${message}`, ...args)
+}
 
-    return (
-        <div className="w-[300px] p-4 text-center">
-            <h1 className="text-2xl font-bold mb-4">WXT React Popup</h1>
-            <div className="p-8">
-                <Button onClick={() => setCount(count + 1)} variant="outline">
-                    Count is {count}
-                </Button>
-                <p className="mt-4">
-                    Edit{' '}
-                    <code className="bg-gray-100 px-1 rounded">
-                        popup/main.tsx
-                    </code>{' '}
-                    and save to test HMR
-                </p>
-            </div>
-        </div>
+interface Member {
+    userId: string
+}
+
+interface RoomState {
+    cells?: any[]
+    nytogetherState?: {
+        userId?: string
+        members?: { [name: string]: Member }
+    }
+}
+
+function sendMessageToTab(messageID: string, data: any) {
+    browser.tabs.query({ active: true, currentWindow: true }, (tabs: any) => {
+        if (tabs.length > 0) {
+            sendMessage(messageID, data, `content-script@${tabs[0].id}`)
+        }
+    })
+}
+
+function useRoomState() {
+    const [roomState, setRoomState] = React.useState<RoomState | null>(null)
+
+    React.useEffect(() => {
+        const unlisten = onMessage('room-state', (message) => {
+            log('Received room state message', message)
+
+            // the room state update from the content script can be partial,
+            // so we merge it with the current state
+            setRoomState((prev) => ({
+                ...prev,
+                ...(message.data as RoomState),
+            }))
+        })
+
+        sendMessageToTab('query-room-state', {})
+        log('Listening for room state messages')
+        return unlisten
+    }, [setRoomState])
+
+    const customSetRoomState = React.useCallback(
+        (newState: RoomState) => {
+            sendMessageToTab('set-room-state', newState)
+        },
+        [sendMessageToTab]
     )
+    return { roomState, setNYTogetherState: customSetRoomState }
+}
+
+function Contents() {
+    const { roomState, setNYTogetherState } = useRoomState()
+
+    if (roomState === null) {
+        return (
+            <Alert>
+                <AlertTitle>No puzzle found</AlertTitle>
+                <AlertDescription>
+                    Open a NYT crossword puzzle to get started!
+                </AlertDescription>
+            </Alert>
+        )
+    }
+    return <p>Hello {roomState.nytogetherState?.userId}</p>
 }
 
 // Mount React app
 const root = createRoot(document.getElementById('app')!)
 root.render(
     <React.StrictMode>
-        <Popup />
+        <div className="w-[300px] p-4 text-center">
+            <h1 className="text-2xl font-bold mb-4">NYTogether</h1>
+            <Contents />
+        </div>
     </React.StrictMode>
 )
