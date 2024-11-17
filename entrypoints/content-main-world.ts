fe325b6aca95bb85ec8d718f444c5c2dc43a7929
Kevin Wu <kevin@kevinniuwu.com>
2024-11-17 00:42:10 -0500

    Toggle pencil on caps lock

--- entrypoints/content-main-world.ts
+++ entrypoints/content-main-world.ts
@@ -16,6 +16,52 @@ interface NYTUser {
     regiId: string
 }
 
+let globalStore: any = null
+
+interface NYTStoreState {
+    cells: [
+        {
+            checked: boolean
+            clues: [number]
+            confirmed: boolean
+            guess: string
+            index: number
+            modified: boolean
+            penciled: boolean
+            revealed: boolean
+            type: number
+        }
+    ]
+    clues: [
+        {
+            alternativeAriaLabelText: string
+            cells: [number]
+            direction: 'Across' | 'Down'
+            index: number
+            isImageClue: boolean
+            label: string
+            list: number
+            next: number
+            prev: number
+            text: string
+            unfilledCount: number
+        }
+    ]
+    toolbar: {
+        inPencilMode: boolean
+        inRebusMode: boolean
+        rebusValue: string
+    }
+    status: {
+        autocheckEnabled: boolean
+        blankCells: number
+        currentProgress: number
+        incorrectCells: number
+        isFilled: boolean
+        isSolved: boolean
+    }
+}
+
 function isNYTUser(user: any): user is NYTUser {
     return (
         typeof user === 'object' &&
@@ -140,6 +186,8 @@ const handleGameStore = (elem: Element): boolean => {
         sendState()
     })
 
+    globalStore = store
+
     onMessage('query-room-state', (message) => {
         log('Querying room-state')
         sendState()
@@ -167,4 +215,22 @@ export default defineUnlistedScript(() => {
 
     observeElement('#hub-root > div.hub-welcome', handleRedeem)
     observeElement('main', handleGameStore)
+
+    // Add caps lock detection
+    const handleCapsLock = (event: KeyboardEvent) => {
+        if (event.code === 'CapsLock') {
+            const capsLockState = event.getModifierState('CapsLock')
+            log(`Caps Lock ${capsLockState ? 'ON' : 'OFF'}`)
+            const state = globalStore?.getState() as NYTStoreState
+            log(`Pencil mode: ${state.toolbar.inPencilMode}`)
+            if (state.toolbar.inPencilMode !== capsLockState) {
+                globalStore?.dispatch({
+                    type: 'crossword/toolbar/TOGGLE_PENCIL_MODE',
+                })
+            }
+        }
+    }
+
+    document.addEventListener('keydown', handleCapsLock)
+    document.addEventListener('keyup', handleCapsLock)
 })
