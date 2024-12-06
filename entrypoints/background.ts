393b4972f60a6bc3eb88adbff547260368a60009
Kevin Wu <kevin@kevinniuwu.com>
2024-12-06 09:21:05 -0500

    Update icon/badge based on room state

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,4 +1,7 @@
+import { RoomState } from '@/lib/nyt-interfaces'
 import { onMessage } from 'webext-bridge/background'
+import aloneIconUrl from '@/assets/alone-48.png'
+import inRoomIconUrl from '@/assets/together-48.png'
 
 export default defineBackground(() => {
     console.log('Hello background!', { id: browser.runtime.id })
@@ -11,6 +14,37 @@ export default defineBackground(() => {
 
     onMessage('room-state', async (message) => {
         console.log('Received room state:', message)
-        console.log('Sent from tab id:', message.sender.tabId)
+        const tabId = message.sender.tabId
+        if (!tabId) {
+            console.error('No tab id in message sender')
+            return
+        }
+
+        console.log('Sent from tab id:', tabId)
+        const roomState = message.data as unknown as RoomState
+
+        try {
+            if (roomState) {
+                await browser.action.setIcon({
+                    path: inRoomIconUrl,
+                    tabId,
+                })
+                await browser.action.setBadgeText({
+                    text: Object.keys(roomState.members).length.toString(),
+                    tabId,
+                })
+            } else {
+                await browser.action.setBadgeText({
+                    text: null,
+                    tabId,
+                })
+                await browser.action.setIcon({
+                    path: aloneIconUrl,
+                    tabId,
+                })
+            }
+        } catch (error) {
+            console.error('Error setting icon or badge text:', error)
+        }
     })
 })
