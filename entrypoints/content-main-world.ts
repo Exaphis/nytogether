a30d54272d7e3ae64a529ee87c7a00cf56a89eaa
Kevin Wu <kevin@kevinniuwu.com>
2024-12-06 11:03:02 -0500

    Inline findReact

--- entrypoints/content-main-world.ts
+++ entrypoints/content-main-world.ts
@@ -1,4 +1,3 @@
-import { findReact } from '@/utils'
 import { sendMessage, setNamespace, onMessage } from 'webext-bridge/window'
 import type {
     NYTUser,
@@ -13,6 +12,32 @@ const log = (message: string, ...args: any[]) => {
 
 const error = (message: string, ...args: any[]) => {
     console.error(`[NYTogether/content-main-world] ${message}`, ...args)
+}
+
+// https://stackoverflow.com/q/47166101
+function findReact(dom: any, traverseUp = 0) {
+    const key = Object.keys(dom).find((key) => {
+        return (
+            key.startsWith('__reactFiber$') || // react 17+
+            key.startsWith('__reactInternalInstance$')
+        ) // react <17
+    })!
+    const domFiber = dom[key]
+    if (domFiber == null) return null
+
+    // react 16+
+    const GetCompFiber = (fiber: any) => {
+        let parentFiber = fiber.return
+        while (typeof parentFiber.type == 'string') {
+            parentFiber = parentFiber.return
+        }
+        return parentFiber
+    }
+    let compFiber = GetCompFiber(domFiber)
+    for (let i = 0; i < traverseUp; i++) {
+        compFiber = GetCompFiber(compFiber)
+    }
+    return compFiber
 }
 
 class GameState {
